b90c390445456b5fd153bce71e0df75ab7350bf1
leomoraesitu <leomoraesitu@hotmail.com>
2024-09-24 19:47:48 -0300

    [FIX]functions

--- bin/calculadora.dart
+++ bin/calculadora.dart
@@ -1,47 +1,61 @@
 import 'dart:convert';
 import 'dart:io';
 
-void main(List<String> arguments){
+void main(List<String> arguments) {
   print("CALCULADORA");
 
-  print("Informe o primeiro número:");
-  var line = stdin.readLineSync(encoding: utf8);
-  var numero1 = double.parse(line ?? "0");
+  var numero1 = double.parse(lerConsole("Informe o primeiro número:"));
 
-  print("Informe o segundo número:");
-  line = stdin.readLineSync(encoding: utf8);
-  var numero2 = double.parse(line ?? "0");
+  var numero2 = double.parse(lerConsole("Informe o segundo número:"));
 
-  print("Informe a operação mateática (+, -, *, /):");
-  line = stdin.readLineSync(encoding: utf8);
-  var operacao = line ?? "0";
+  var operacao = lerConsole("Informe a operação mateática (+, -, *, /):");
 
-  print (numero1);
-  print (numero2);
-  print (operacao);
+  calcular(operacao, numero1, numero2);
+}
 
+void calcular(String operacao, double numero1, double numero2) {
   double resultado = 0;
+  switch (operacao) {
+    case "+":
+      resultado = soma(numero1, numero2);
+      break;
+    case "-":
+      resultado = subtracao(numero1, numero2);
+      break;
+    case "*":
+      resultado = multiplicacao(numero1, numero2);
+      break;
+    case "/":
+      resultado = divisao(numero1, numero2);
+      break;
 
-  switch(operacao){
-    case "+": 
-    resultado  = numero1 + numero2;
-    break;
-    case "-": 
-    resultado  = numero1 - numero2;
-    break;
-    case "*": 
-    resultado  = numero1 * numero2;
-    break;
-    case "/": 
-    resultado  = numero1 / numero2;
-    break;
-    
     default:
-    print("Operação inválida!");
-    exit(0);
+      print("Operação inválida!");
+      exit(0);
   }
-  
+
   print("Operação solicitada: $operacao");
   print("O resultado é: $resultado");
+}
 
+String lerConsole(String texto) {
+  print(texto);
+  var line = stdin.readLineSync(encoding: utf8);
+  return line ?? "";
+}
+
+double soma(double numero1, double numero2) {
+  return numero1 + numero2;
+}
+
+double subtracao(double numero1, double numero2) {
+  return numero1 - numero2;
+}
+
+double multiplicacao(double numero1, double numero2) {
+  return numero1 * numero2;
+}
+
+double divisao(double numero1, double numero2) {
+  return numero1 / numero2;
 }
